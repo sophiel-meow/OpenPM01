3f6d5aa5d2f5be500ed34ed1b128e52bd01373cf
Sophiel Zhou <sophiel@zzy040330.moe>
2026-05-24 15:53:02 +0800

    periph: fine-grained delay for soft spi

--- src/st7789_stm32_spi.c
+++ src/st7789_stm32_spi.c
@@ -26,13 +26,19 @@ SOFTWARE.
 
 #include "st7789_stm32_spi.h"
 
+extern volatile uint32_t sys_ticks;
+
+static void _st_delay_ms(uint32_t ms) {
+    uint32_t end = sys_ticks + ms;
+    while (sys_ticks < end)
+        ;
+}
+
 // Display dimensions: 172x320
 uint16_t st_tftwidth = 172;
 uint16_t st_tftheight = 320;
 
-
-void st_set_address_window(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
-{
+void st_set_address_window(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
     uint16_t xs = x1 + ST_X_OFFSET;
     uint16_t xe = x2 + ST_X_OFFSET;
     uint16_t ys = y1 + ST_Y_OFFSET;
@@ -58,9 +64,8 @@ void st_set_address_window(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
     _st_write_command_8bit(ST7789_RAMWR);
 }
 
-
-void _st_render_glyph(uint16_t x, uint16_t y, uint16_t fore_color, uint16_t back_color, const tImage *glyph, uint8_t is_bg)
-{
+void _st_render_glyph(uint16_t x, uint16_t y, uint16_t fore_color,
+                      uint16_t back_color, const tImage *glyph, uint8_t is_bg) {
     /* Column-burst: one address window per column, all pixels in one SPI burst.
        Eliminates the per-pixel CASET/RASET/RAMWR overhead (~14 bytes per pixel)
        and the visible scan-line tearing it causes. */
@@ -85,9 +90,11 @@ void _st_render_glyph(uint16_t x, uint16_t y, uint16_t fore_color, uint16_t back
             int n = bits_left < 8 ? bits_left : 8;
             for (int bit = 0; bit < n; bit++) {
                 if (d & 0x80) {
-                    ST_WRITE_8BIT(bh); ST_WRITE_8BIT(bl);
+                    ST_WRITE_8BIT(bh);
+                    ST_WRITE_8BIT(bl);
                 } else {
-                    ST_WRITE_8BIT(fh); ST_WRITE_8BIT(fl);
+                    ST_WRITE_8BIT(fh);
+                    ST_WRITE_8BIT(fl);
                 }
                 d <<= 1;
             }
@@ -97,9 +104,9 @@ void _st_render_glyph(uint16_t x, uint16_t y, uint16_t fore_color, uint16_t back
     }
 }
 
-
-void _st_draw_string_main(uint16_t x, uint16_t y, char *str, uint16_t fore_color, uint16_t back_color, const tFont *font, uint8_t is_bg)
-{
+void _st_draw_string_main(uint16_t x, uint16_t y, char *str,
+                          uint16_t fore_color, uint16_t back_color,
+                          const tFont *font, uint8_t is_bg) {
     uint16_t x_temp = x;
     uint16_t y_temp = y;
     uint8_t x_padding = 0;
@@ -107,23 +114,15 @@ void _st_draw_string_main(uint16_t x, uint16_t y, char *str, uint16_t fore_color
     const tImage *img = NULL;
     uint16_t width = 0, height = 0;
 
-    while (*str)
-    {
-        if (*str == '\n')
-        {
+    while (*str) {
+        if (*str == '\n') {
             x_temp = x;
             y_temp += (font->chars[0].image->height + y_padding);
-        }
-        else if (*str == '\t')
-        {
+        } else if (*str == '\t') {
             x_temp += 4 * (font->chars[0].image->height + y_padding);
-        }
-        else
-        {
-            for (uint8_t i = 0; i < font->length; i++)
-            {
-                if (font->chars[i].code == *str)
-                {
+        } else {
+            for (uint8_t i = 0; i < font->length; i++) {
+                if (font->chars[i].code == *str) {
                     img = font->chars[i].image;
                     break;
                 }
@@ -136,16 +135,17 @@ void _st_draw_string_main(uint16_t x, uint16_t y, char *str, uint16_t fore_color
 
             if (y_temp + (height + y_padding) > st_tftheight - 1)
                 return;
-            if (x_temp + (width + x_padding) > st_tftwidth - 1)
-            {
+            if (x_temp + (width + x_padding) > st_tftwidth - 1) {
                 x_temp = x;
                 y_temp += (height + y_padding);
             }
 
             if (is_bg)
-                _st_render_glyph(x_temp, y_temp, fore_color, back_color, img, 1);
+                _st_render_glyph(x_temp, y_temp, fore_color, back_color, img,
+                                 1);
             else
-                _st_render_glyph(x_temp, y_temp, fore_color, back_color, img, 0);
+                _st_render_glyph(x_temp, y_temp, fore_color, back_color, img,
+                                 0);
             x_temp += (width + x_padding);
         }
 
@@ -153,14 +153,11 @@ void _st_draw_string_main(uint16_t x, uint16_t y, char *str, uint16_t fore_color
     }
 }
 
-
-void st_draw_char(uint16_t x, uint16_t y, char character, uint16_t fore_color, uint16_t back_color, const tFont *font, uint8_t is_bg)
-{
+void st_draw_char(uint16_t x, uint16_t y, char character, uint16_t fore_color,
+                  uint16_t back_color, const tFont *font, uint8_t is_bg) {
     const tImage *img = NULL;
-    for (uint8_t i = 0; i < font->length; i++)
-    {
-        if (font->chars[i].code == character)
-        {
+    for (uint8_t i = 0; i < font->length; i++) {
+        if (font->chars[i].code == character) {
             img = font->chars[i].image;
             break;
         }
@@ -174,21 +171,18 @@ void st_draw_char(uint16_t x, uint16_t y, char character, uint16_t fore_color, u
         _st_render_glyph(x, y, fore_color, back_color, img, 0);
 }
 
-
-void st_draw_string(uint16_t x, uint16_t y, char *str, uint16_t color, const tFont *font)
-{
+void st_draw_string(uint16_t x, uint16_t y, char *str, uint16_t color,
+                    const tFont *font) {
     _st_draw_string_main(x, y, str, color, 0, font, 0);
 }
 
-
-void st_draw_string_withbg(uint16_t x, uint16_t y, char *str, uint16_t fore_color, uint16_t back_color, const tFont *font)
-{
+void st_draw_string_withbg(uint16_t x, uint16_t y, char *str,
+                           uint16_t fore_color, uint16_t back_color,
+                           const tFont *font) {
     _st_draw_string_main(x, y, str, fore_color, back_color, font, 1);
 }
 
-
-void st_draw_bitmap(uint16_t x, uint16_t y, const tImage *bitmap)
-{
+void st_draw_bitmap(uint16_t x, uint16_t y, const tImage *bitmap) {
     uint16_t width = bitmap->width;
     uint16_t height = bitmap->height;
     st_set_address_window(x, y, x + width - 1, y + height - 1);
@@ -197,8 +191,7 @@ void st_draw_bitmap(uint16_t x, uint16_t y, const tImage *bitmap)
     ST_DC_DAT;
 
     uint32_t total_pixels = (uint32_t)width * (uint32_t)height;
-    for (uint32_t pixels = 0; pixels < total_pixels; pixels++)
-    {
+    for (uint32_t pixels = 0; pixels < total_pixels; pixels++) {
         ST_WRITE_8BIT((uint8_t)(bitmap->data[2 * pixels]));
         ST_WRITE_8BIT((uint8_t)(bitmap->data[2 * pixels + 1]));
     }
@@ -206,9 +199,7 @@ void st_draw_bitmap(uint16_t x, uint16_t y, const tImage *bitmap)
     ST_CS_IDLE;
 }
 
-
-void st_fill_color(uint16_t color, uint32_t len)
-{
+void st_fill_color(uint16_t color, uint32_t len) {
     ST_CS_ACTIVE;
     ST_DC_DAT;
 
@@ -216,10 +207,14 @@ void st_fill_color(uint16_t color, uint32_t len)
     uint32_t tail = len % 8;
 
     while (blocks--) {
-        ST_WRITE_16BIT(color); ST_WRITE_16BIT(color);
-        ST_WRITE_16BIT(color); ST_WRITE_16BIT(color);
-        ST_WRITE_16BIT(color); ST_WRITE_16BIT(color);
-        ST_WRITE_16BIT(color); ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
+        ST_WRITE_16BIT(color);
     }
     while (tail--)
         ST_WRITE_16BIT(color);
@@ -227,9 +222,8 @@ void st_fill_color(uint16_t color, uint32_t len)
     ST_CS_IDLE;
 }
 
-
-void st_fill_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color)
-{
+void st_fill_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h,
+                  uint16_t color) {
     if (x >= st_tftwidth || y >= st_tftheight || w == 0 || h == 0)
         return;
     if (x + w - 1 >= st_tftwidth)
@@ -241,23 +235,19 @@ void st_fill_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color
     st_fill_color(color, (uint32_t)w * (uint32_t)h);
 }
 
-
-void st_fill_rect_fast(uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t color)
-{
+void st_fill_rect_fast(uint16_t x1, uint16_t y1, uint16_t w, uint16_t h,
+                       uint16_t color) {
     st_set_address_window(x1, y1, x1 + w - 1, y1 + h - 1);
     st_fill_color(color, (uint32_t)w * (uint32_t)h);
 }
 
-
-void st_fill_screen(uint16_t color)
-{
+void st_fill_screen(uint16_t color) {
     st_set_address_window(0, 0, st_tftwidth - 1, st_tftheight - 1);
     st_fill_color(color, (uint32_t)st_tftwidth * (uint32_t)st_tftheight);
 }
 
-
-void st_draw_rectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color)
-{
+void st_draw_rectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h,
+                       uint16_t color) {
     if (x >= st_tftwidth || y >= st_tftheight || w == 0 || h == 0)
         return;
     if (x + w - 1 >= st_tftwidth)
@@ -271,9 +261,8 @@ void st_draw_rectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t 
     _st_draw_fast_v_line(x + w, y, y + h - 1, 1, color);
 }
 
-
-void _st_plot_line_low(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t width, uint16_t color)
-{
+void _st_plot_line_low(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
+                       uint8_t width, uint16_t color) {
     int16_t dx = x1 - x0;
     int16_t dy = y1 - y0;
     int8_t yi = 1;
@@ -281,33 +270,36 @@ void _st_plot_line_low(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8
     uint8_t color_high = (uint8_t)(color >> 8);
     uint8_t color_low = (uint8_t)color;
 
-    if (dy < 0) { yi = -1; dy = -dy; }
+    if (dy < 0) {
+        yi = -1;
+        dy = -dy;
+    }
 
     int16_t D = 2 * dy - dx;
     uint16_t y = y0;
     uint16_t x = x0;
 
-    while (x <= x1)
-    {
+    while (x <= x1) {
         st_set_address_window(x, y, x + width - 1, y + width - 1);
         ST_CS_ACTIVE;
         ST_DC_DAT;
-        for (uint8_t pixel_cnt = 0; pixel_cnt < pixels_per_point; pixel_cnt++)
-        {
+        for (uint8_t pixel_cnt = 0; pixel_cnt < pixels_per_point; pixel_cnt++) {
             ST_WRITE_8BIT(color_high);
             ST_WRITE_8BIT(color_low);
         }
         ST_CS_IDLE;
 
-        if (D > 0) { y = y + yi; D = D - 2 * dx; }
+        if (D > 0) {
+            y = y + yi;
+            D = D - 2 * dx;
+        }
         D = D + 2 * dy;
         x++;
     }
 }
 
-
-void _st_plot_line_high(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t width, uint16_t color)
-{
+void _st_plot_line_high(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
+                        uint8_t width, uint16_t color) {
     int16_t dx = x1 - x0;
     int16_t dy = y1 - y0;
     int8_t xi = 1;
@@ -315,52 +307,47 @@ void _st_plot_line_high(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint
     uint8_t color_high = (uint8_t)(color >> 8);
     uint8_t color_low = (uint8_t)color;
 
-    if (dx < 0) { xi = -1; dx = -dx; }
+    if (dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
 
     int16_t D = 2 * dx - dy;
     uint16_t y = y0;
     uint16_t x = x0;
 
-    while (y <= y1)
-    {
+    while (y <= y1) {
         st_set_address_window(x, y, x + width - 1, y + width - 1);
         ST_CS_ACTIVE;
         ST_DC_DAT;
-        for (uint8_t pixel_cnt = 0; pixel_cnt < pixels_per_point; pixel_cnt++)
-        {
+        for (uint8_t pixel_cnt = 0; pixel_cnt < pixels_per_point; pixel_cnt++) {
             ST_WRITE_8BIT(color_high);
             ST_WRITE_8BIT(color_low);
         }
         ST_CS_IDLE;
 
-        if (D > 0) { x = x + xi; D = D - 2 * dy; }
+        if (D > 0) {
+            x = x + xi;
+            D = D - 2 * dy;
+        }
         D = D + 2 * dx;
         y++;
     }
 }
 
-
-void st_draw_line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t width, uint16_t color)
-{
-    if (x0 == x1)
-    {
+void st_draw_line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
+                  uint8_t width, uint16_t color) {
+    if (x0 == x1) {
         _st_draw_fast_v_line(x0, y0, y1, width, color);
-    }
-    else if (y0 == y1)
-    {
+    } else if (y0 == y1) {
         _st_draw_fast_h_line(x0, y0, x1, width, color);
-    }
-    else
-    {
-        if (abs(y1 - y0) < abs(x1 - x0))
-        {
+    } else {
+        if (abs(y1 - y0) < abs(x1 - x0)) {
             if (x0 > x1)
                 _st_plot_line_low(x1, y1, x0, y0, width, color);
             else
                 _st_plot_line_low(x0, y0, x1, y1, width, color);
-        }
-        else
-        {
+        } else {
             if (y0 > y1)
                 _st_plot_line_high(x1, y1, x0, y0, width, color);
             else
@@ -369,9 +356,8 @@ void st_draw_line(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t wi
     }
 }
 
-
-void _st_draw_fast_h_line(uint16_t x0, uint16_t y0, uint16_t x1, uint8_t width, uint16_t color)
-{
+void _st_draw_fast_h_line(uint16_t x0, uint16_t y0, uint16_t x1, uint8_t width,
+                          uint16_t color) {
     if (x0 < x1)
         st_set_address_window(x0, y0, x1, y0 + width - 1);
     else
@@ -379,9 +365,8 @@ void _st_draw_fast_h_line(uint16_t x0, uint16_t y0, uint16_t x1, uint8_t width, 
     st_fill_color(color, (uint32_t)width * (uint32_t)abs(x1 - x0 + 1));
 }
 
-
-void _st_draw_fast_v_line(uint16_t x0, uint16_t y0, uint16_t y1, uint8_t width, uint16_t color)
-{
+void _st_draw_fast_v_line(uint16_t x0, uint16_t y0, uint16_t y1, uint8_t width,
+                          uint16_t color) {
     if (y0 < y1)
         st_set_address_window(x0, y0, x0 + width - 1, y1);
     else
@@ -389,9 +374,7 @@ void _st_draw_fast_v_line(uint16_t x0, uint16_t y0, uint16_t y1, uint8_t width, 
     st_fill_color(color, (uint32_t)width * (uint32_t)abs(y1 - y0 + 1));
 }
 
-
-void st_draw_pixel(uint16_t x, uint16_t y, uint16_t color)
-{
+void st_draw_pixel(uint16_t x, uint16_t y, uint16_t color) {
     st_set_address_window(x, y, x, y);
     ST_CS_ACTIVE;
     ST_DC_DAT;
@@ -400,65 +383,63 @@ void st_draw_pixel(uint16_t x, uint16_t y, uint16_t color)
     ST_CS_IDLE;
 }
 
-
-void st_rotate_display(uint8_t rotation)
-{
+void st_rotate_display(uint8_t rotation) {
     rotation = rotation % 4;
     _st_write_command_8bit(ST7789_MADCTL);
 
-    switch (rotation)
-    {
-        case 0:
-            // Portrait: 172 wide, 320 tall
-            _st_write_data_8bit(ST7789_MADCTL_RGB);
-            st_tftwidth = 172;
-            st_tftheight = 320;
-            break;
-        case 1:
-            // Landscape 1: 320 wide, 172 tall
-            _st_write_data_8bit(ST7789_MADCTL_MX | ST7789_MADCTL_MY | ST7789_MADCTL_RGB);
-            st_tftwidth = 320;
-            st_tftheight = 172;
-            break;
-        case 2:
-            // Portrait inverted
-            _st_write_data_8bit(ST7789_MADCTL_MY | ST7789_MADCTL_MV | ST7789_MADCTL_RGB);
-            st_tftwidth = 172;
-            st_tftheight = 320;
-            break;
-        case 3:
-            // Landscape 2
-            _st_write_data_8bit(ST7789_MADCTL_MX | ST7789_MADCTL_MV | ST7789_MADCTL_RGB);
-            st_tftwidth = 320;
-            st_tftheight = 172;
-            break;
+    switch (rotation) {
+    case 0:
+        // Portrait: 172 wide, 320 tall
+        _st_write_data_8bit(ST7789_MADCTL_RGB);
+        st_tftwidth = 172;
+        st_tftheight = 320;
+        break;
+    case 1:
+        // Landscape 1: 320 wide, 172 tall
+        _st_write_data_8bit(ST7789_MADCTL_MX | ST7789_MADCTL_MY |
+                            ST7789_MADCTL_RGB);
+        st_tftwidth = 320;
+        st_tftheight = 172;
+        break;
+    case 2:
+        // Portrait inverted
+        _st_write_data_8bit(ST7789_MADCTL_MY | ST7789_MADCTL_MV |
+                            ST7789_MADCTL_RGB);
+        st_tftwidth = 172;
+        st_tftheight = 320;
+        break;
+    case 3:
+        // Landscape 2
+        _st_write_data_8bit(ST7789_MADCTL_MX | ST7789_MADCTL_MV |
+                            ST7789_MADCTL_RGB);
+        st_tftwidth = 320;
+        st_tftheight = 172;
+        break;
     }
 }
 
-
-void st_init(void)
-{
+void st_init(void) {
     ST_CONFIG_GPIO_CLOCK();
     ST_CONFIG_GPIO();
 
     // Hardware reset
     ST_RST_ACTIVE;
-    _st_fixed_delay();
+    _st_delay_ms(1);
     ST_RST_IDLE;
-    _st_fixed_delay();
+    _st_delay_ms(5);
 
     // Software reset
     _st_write_command_8bit(ST7789_SWRESET);
-    _st_fixed_delay();
+    _st_delay_ms(5);
 
     // Sleep out
     _st_write_command_8bit(ST7789_SLPOUT);
-    _st_fixed_delay();
+    _st_delay_ms(5);
 
     // Color mode: 65K, 16-bit RGB565
     _st_write_command_8bit(ST7789_COLMOD);
     _st_write_data_8bit(ST7789_COLOR_MODE_65K | ST7789_COLOR_MODE_16BIT);
-    _st_fixed_delay();
+    _st_delay_ms(1);
 
     // MADCTL: Row/Column order
     _st_write_command_8bit(ST7789_MADCTL);
@@ -466,7 +447,7 @@ void st_init(void)
 
     // Porch control
     _st_write_command_8bit(ST7789_PORCTRL);
-    _st_write_data_8bit(0x0C);  // Back porch [2:0]=4, Front porch [2:0]=4
+    _st_write_data_8bit(0x0C); // Back porch [2:0]=4, Front porch [2:0]=4
     _st_write_data_8bit(0x0C);
     _st_write_data_8bit(0x00);
     _st_write_data_8bit(0x33);
@@ -474,11 +455,11 @@ void st_init(void)
 
     // Gate control
     _st_write_command_8bit(ST7789_GCTRL);
-    _st_write_data_8bit(0x35);  // VGH=13.26V, VGL=-10.43V
+    _st_write_data_8bit(0x35); // VGH=13.26V, VGL=-10.43V
 
     // VCOM setting
     _st_write_command_8bit(ST7789_VCOMS);
-    _st_write_data_8bit(0x28);  // VCOM=0.9V
+    _st_write_data_8bit(0x28); // VCOM=0.9V
 
     // LCM control
     _st_write_command_8bit(ST7789_LCMCTRL);
@@ -499,7 +480,7 @@ void st_init(void)
 
     // Frame rate control
     _st_write_command_8bit(ST7789_FRCTRL2);
-    _st_write_data_8bit(0x0F);  // 60Hz
+    _st_write_data_8bit(0x0F); // 60Hz
 
     // Power control 1
     _st_write_command_8bit(ST7789_PWCTRL1);
@@ -542,23 +523,16 @@ void st_init(void)
 
     // Inversion on
     _st_write_command_8bit(ST7789_INVON);
-    _st_fixed_delay();
+    _st_delay_ms(1);
 
     // Normal display on
     _st_write_command_8bit(ST7789_NORON);
-    _st_fixed_delay();
+    _st_delay_ms(1);
 
     // Display on
     _st_write_command_8bit(ST7789_DISPON);
-    _st_fixed_delay();
+    _st_delay_ms(1);
 
     // Turn on backlight
     ST_BLK_ON;
-}
-
-
-void _st_fixed_delay(void)
-{
-    for (uint16_t i = 0; i < 5000; i++)
-        __asm__("nop");
 }
